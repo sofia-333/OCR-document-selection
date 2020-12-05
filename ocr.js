c67002a9d06d7ff074b53a4940af1d5c61914f8d
sofia-333 <sofio.muradovi@gmail.com>
2020-12-05 22:49:31 +0400

    changed cursor starting point

--- ocr.js
+++ ocr.js
@@ -3,42 +3,44 @@ const docImage = document.getElementsByClassName("doc-image")[0];
 const container = document.getElementsByClassName("container")[0];
 var posX, posY, newDiv;
 let x1, x2, dropped = true;
+var divLayer;
 
-// div which will be covering the image
-const divLayer = document.getElementsByClassName("div-layer")[0];
-divLayer.style.width = docImage.width + "px";
-divLayer.style.height = docImage.height + "px";
-divLayer.x = docImage.x;
-divLayer.y = docImage.y;
+setTimeout(() => {
+  // div which will be covering the image
+  divLayer = document.getElementsByClassName("div-layer")[0];
+  divLayer.style.width = docImage.width + "px";
+  divLayer.style.height = docImage.height + "px";
+  divLayer.x = docImage.x;
+  divLayer.y = docImage.y;
 
-divLayer.onmousedown = function (e) {
-  dropped = false;
-  console.log(e.pageY);
-  posX = e.pageX + 5;
-  posY = e.pageY + 5;
-  //coordinates relative to image(coordinates to send)
-  x1 = posX - docImage.x;
-  x2 = posY - docImage.y;
-  newDiv = document.createElement('div');
-  newDiv.classList.add("new-div");
-  //div-s real coordinates are relative to this page
-  newDiv.style.left = posX + "px";
-  newDiv.style.top = posY + "px";
-  container.appendChild(newDiv);
-};
-divLayer.onmousemove = function (e) {
-  if (!dropped) {
-    console.log("moved");
-    let divWidth = e.pageX - posX;
-    let divHeight = e.pageY - posY;
-    newDiv.style.width = divWidth + "px";
-    newDiv.style.height = divHeight + "px";
-  }
-};
-document.onmouseup = function (e) {
-  if (newDiv.style.height !== "0px" && newDiv.style.width !== "0px") {
-    dropped = true;
-  } else {
-    container.removeChild(container.lastElementChild);
-  }
-}; 
+  divLayer.onmousedown = function (e) {
+    dropped = false;
+    posX = e.pageX - 10;
+    posY = e.pageY - 10;
+    //coordinates relative to image(coordinates to send)
+    x1 = posX - docImage.x;
+    x2 = posY - docImage.y;
+    newDiv = document.createElement('div');
+    newDiv.classList.add("new-div");
+    //div-s real coordinates are relative to this page
+    newDiv.style.left = posX + "px";
+    newDiv.style.top = posY + "px";
+    container.appendChild(newDiv);
+  };
+  divLayer.onmousemove = function (e) {
+    if (!dropped) {
+      console.log("moved");
+      let divWidth = e.pageX - posX;
+      let divHeight = e.pageY - posY;
+      newDiv.style.width = divWidth + "px";
+      newDiv.style.height = divHeight + "px";
+    }
+  };
+  document.onmouseup = function (e) {
+    if (newDiv.style.height !== "0px" && newDiv.style.width !== "0px") {
+      dropped = true;
+    } else {
+      container.removeChild(container.lastElementChild);
+    }
+  };
+}, 100);
