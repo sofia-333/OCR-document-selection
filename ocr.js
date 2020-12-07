a48cf11d482072bc335bde0bcb159f4ea3fccaef
sofia-333 <sofio.muradovi@gmail.com>
2020-12-07 17:17:14 +0400

    added dropdowns

--- ocr.js
+++ ocr.js
@@ -1,10 +1,11 @@
 const submitButton = document.getElementById("submitButton");
+const deleteButton = document.getElementById("deleteButton");
 const docImage = document.getElementsByClassName("doc-image")[0];
 const container = document.getElementsByClassName("container")[0];
 let posX, posY, newDiv, dropped = true, x1, y1, divHeight, divWidth, divLayer;
 const dataToSend = new Array;
 
-docImage.onload = function() {
+docImage.onload = function () {
   // div which will be covering the image
   divLayer = document.getElementsByClassName("div-layer")[0];
   divLayer.style.width = docImage.width + "px";
@@ -32,22 +33,37 @@ docImage.onload = function() {
       divHeight = e.pageY - posY - 10;
       newDiv.style.width = divWidth + "px";
       newDiv.style.height = divHeight + "px";
+
     }
   };
   document.onmouseup = function (e) {
-    if (newDiv.style.height !== "" && newDiv.style.width !== "") {
+    //!dropped - means that new dropdown won't be created when click on div
+    if (newDiv.style.height !== "" && newDiv.style.width !== "" && !dropped) {
+      //creating dropdowns
+      const dropdown = document.createElement("select");
+      dropdown.classList.add("dropdown");
+      dropdown.style.width = divWidth + "px";
+      dropdown.style.height = divHeight + "px";
+      for (let i = 0; i < 3; i++) {
+        let option = new Option;
+        option.text = `something ${i}`;
+        option.value = 'somethings value';
+        dropdown.options.add(option);
+      }
+      newDiv.appendChild(dropdown);
       dropped = true;
       //Create new JSON object and add it to array
       const parameters = {
-        left : x1,
-        top : y1,
-        height : newDiv.style.height,
-        width : newDiv.style.width,
+        left: x1,
+        top: y1,
+        height: newDiv.style.height,
+        width: newDiv.style.width,
       };
       dataToSend.push(parameters);
-    } else {
-      container.removeChild(container.lastElementChild);
     }
+    //else {
+    //   container.removeChild(container.lastElementChild);
+    // }
   };
 
   submitButton.addEventListener('click', (e) => {
