5ae71da74d9de068597f65d36f30572d576ef8e7
sofia-333 <sofio.muradovi@gmail.com>
2020-12-10 17:32:09 +0400

    need To fix resize

--- ocr.js
+++ ocr.js
@@ -3,7 +3,7 @@ const deleteButton = document.getElementById("deleteButton");
 const docImage = document.getElementsByClassName("doc-image")[0];
 const container = document.getElementsByClassName("container")[0];
 let posX, posY, newDiv, created = true, x1, y1, divHeight, divWidth, divLayer, outerDiv;
-let dragBtnParent, moved = true, dragPosX , dragPosY, oldDragPosX, oldDragPosY;
+let dragBtnParent, moved = true, dragPosX, dragPosY, oldDragPosX, oldDragPosY,isResizing;
 const dataToSend = new Array;
 
 docImage.onload = () => {
@@ -31,6 +31,7 @@ docImage.onload = () => {
     outerDiv.style.top = posY + "px";
     outerDiv.appendChild(newDiv);
     container.appendChild(outerDiv);
+
     divLayer.onmousemove = function (e) {
       if (!created) {
         divWidth = e.pageX - posX - 10;
@@ -81,7 +82,7 @@ docImage.onload = () => {
         dragButton.getElementsByTagName('i')[0].classList.add("fas");
         dragButton.getElementsByTagName('i')[0].classList.add("fa-arrows-alt");
         outerDiv.appendChild(dragButton);
-        //drag
+        //dragging
         const dragButtons = document.querySelectorAll(".drag-button");
         dragButtons.forEach(btn => {
           btn.addEventListener('mousedown', (e) => {
@@ -96,10 +97,9 @@ docImage.onload = () => {
             else if (e.target.tagName === 'I') {
               dragBtnParent = e.target.parentElement.parentElement;
             }
-            console.log(dragBtnParent);
 
             window.addEventListener('mousemove', (e) => {
-              if (!moved) {
+              if (!moved && !isResizing) {
                 dragPosX = oldDragPosX - e.pageX;
                 dragPosY = oldDragPosY - e.pageY;
                 oldDragPosX = e.pageX;
@@ -112,6 +112,97 @@ docImage.onload = () => {
               moved = true;
             });
           });
+        });
+        //creating resize points
+        //newDiv.classList.add("resizable");
+        let res_se = document.createElement('div');
+        res_se.classList.add("resizers");
+        res_se.classList.add("se");
+        newDiv.appendChild(res_se);
+        let res_ne = document.createElement('div');
+        res_ne.classList.add("resizers");
+        res_ne.classList.add("ne");
+        newDiv.appendChild(res_ne);
+        let res_sw = document.createElement('div');
+        res_sw.classList.add("resizers");
+        res_sw.classList.add("sw");
+        newDiv.appendChild(res_sw);
+        let res_nw = document.createElement('div');
+        res_nw.classList.add("resizers");
+        res_nw.classList.add("nw");
+        newDiv.appendChild(res_nw);
+        //resizing
+
+        const resizers = document.querySelectorAll(".resizers");
+        let currentResizer, currentDropdown, currentDiv, currentDelete,currentDrag;
+        resizers.forEach((resizer) => {
+          resizer.addEventListener("mousedown", mousedown);
+          function mousedown(e) {
+            currentResizer = e.target;
+            currentDiv = e.target.parentElement;
+            currentDropdown = e.target.parentElement.getElementsByClassName("dropdown")[0];
+            currentDelete = e.target.parentElement.parentElement.getElementsByClassName("delete-button")[0];
+            currentDrag = e.target.parentElement.parentElement.getElementsByClassName("drag-button")[0];
+            console.log(currentDiv);
+            isResizing = true;
+
+            let prevX = e.clientX;
+            let prevY = e.clientY;
+
+            window.addEventListener("mousemove", mousemove);
+            window.addEventListener("mouseup", mouseup);
+
+            function mousemove(e) {
+              const divToResize = currentDiv.getBoundingClientRect();
+              if (currentResizer.classList.contains("se")) {
+                currentDiv.style.width = divToResize.width - (prevX - e.clientX) + "px";
+                currentDiv.style.height = divToResize.height - (prevY - e.clientY) + "px";
+                
+                currentDropdown.style.width = divToResize.width - (prevX - e.clientX) + "px";
+                currentDropdown.style.height = divToResize.height - (prevY - e.clientY) + "px";
+              } else if (currentResizer.classList.contains("sw")) {
+                currentDiv.style.width = divToResize.width + (prevX - e.clientX) + "px";
+                currentDiv.style.height = divToResize.height - (prevY - e.clientY) + "px";
+                currentDiv.style.left = divToResize.left - (prevX - e.clientX) + "px";
+                
+                currentDropdown.style.width = divToResize.width + (prevX - e.clientX) + "px";
+                currentDropdown.style.height = divToResize.height - (prevY - e.clientY) + "px";
+                currentDropdown.style.left = divToResize.left - (prevX - e.clientX) + "px";
+              } else if (currentResizer.classList.contains("ne")) {
+                curentDiv.style.width = divToResize.width - (prevX - e.clientX) + "px";
+                curentDiv.style.height = divToResize.height + (prevY - e.clientY) + "px";
+                curentDiv.style.top = divToResize.top - (prevY - e.clientY) + "px";
+                
+                currentDropdown.style.width = divToResize.width - (prevX - e.clientX) + "px";
+                currentDropdown.style.height = divToResize.height + (prevY - e.clientY) + "px";
+                currentDropdown.style.top = divToResize.top - (prevY - e.clientY) + "px";
+              } else {
+                currentDiv.style.width = divToResize.width + (prevX - e.clientX) + "px";
+                currentDiv.style.height = divToResize.height + (prevY - e.clientY) + "px";
+                currentDiv.style.top = divToResize.top - (prevY - e.clientY) + "px";
+                currentDiv.style.left = divToResize.left - (prevX - e.clientX) + "px";
+
+                currentDropdown.style.width = divToResize.width + (prevX - e.clientX) + "px";
+                currentDropdown.style.height = divToResize.height + (prevY - e.clientY) + "px";
+                currentDropdown.style.top = divToResize.top - (prevY - e.clientY) + "px";
+                currentDropdown.style.left = divToResize.left - (prevX - e.clientX) + "px";
+              }
+              currentDelete.style.left = divToResize.width + 1 + "px";
+              currentDelete.style.top = - 5 + "px";
+                
+              currentDrag.style.left = divToResize.width + 1 + "px";
+              currentDrag.style.top = -5 + "px";
+
+              prevX = e.clientX;
+              prevY = e.clientY;
+            }
+
+            function mouseup() {
+              window.removeEventListener("mousemove", mousemove);
+              window.removeEventListener("mouseup", mouseup);
+              isResizing = false;
+            }
+          }
         });
         created = true; //new red div is created
       }
